62737229bdc92a2e6caa8f66fc72c415ad72063b
John <orobosonto@gmail.com>
2023-06-19 03:21:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Retrun: Always 0 (success)
+ */
+init main(void)
+{
+  puts("Programming is like building a multilingual puzzle\n");
+  return (0);
+}
